99bf71b6891d55c582651ec2c899b82049bf32c9
gywn-dev <gyuwonlee.olivia@gmail.com>
2022-01-26 12:00:00 +0900

    스타벅스 랜딩페이지 공지사항 바 및 카드 섹션 마침

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,3 +40,53 @@ window.addEventListener('scroll', _.throttle(function() {
 }, 300)); 
 // _.throttle(함수, 시간(ms))
 // 함수의 실행에 시간만큼의 부하를 줌
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function(fadeEl, index) {
+    gsap.to(fadeEl, 1, {
+        delay: (index + 1) * .7, // 인덱스+1을 곱해주지 않으면 순차적이라는 의미가 없음. 이에 따라 각 요소가 .7초의 간격으로 순차적 출력.
+        opacity: 1
+    });
+});
+
+// new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper-container', { 
+    /* 강사님 화면에는 Swiper가 초록색으로 뜨는데 나는 일반 메소드? 처럼 뜬다. 그리고 overflow-hidden 적용이 안된다... 
+    내가 컨테이너 부분에 가서 직접 해줘야함 뭐지 */
+    direction: 'vertical',
+    autoplay: true,
+    loop: true
+});
+// new Swiper(선택자, 옵션)
+new Swiper('.promotion .swiper-container', {
+    direction: 'horizontal', /* 기본값 */
+    slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+    spaceBetween: 10, // 슬라이드 사이 여백
+    centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+    loop: true,
+    autoplay: {
+        delay: 3000
+    },
+    pagination: {
+        el: '.promotion .swiper-pagination', // 페이지 요소 번호 선택자
+        clickable: true // 사용자의 페이지 번호 요소 제어
+    },
+    navigation: {
+        prevEl: '.promotion .swiper-prev',
+        nextEl: '.promotion .swiper-next'
+    }
+});
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function() {
+    isHidePromotion = !isHidePromotion // false였으면 true로, true였으면 false로 즉 기존 값의반대값으로!
+    if (isHidePromotion) {
+        // 숨김 처리!
+        promotionEl.classList.add('hide');
+    } else {
+        // 보임 처리!
+        promotionEl.classList.remove('hide');
+    }
+});
